381b8e20e874da4159a7d430ef1cd5400e5f4274
harryzan <harryzan@gmail.com>
2012-10-21 23:35:23 +0800

    add new files

---
+++ java/main/oa-web/src/main/java/gov/dsb/core/dao/PersonArrangeDao.java
@@ -0,0 +1,25 @@
+package gov.dsb.core.dao;
+
+import gov.dsb.core.dao.base.EntityService;
+import gov.dsb.core.domain.PersonArrange;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: harryzan
+ * Date: 9/16/12
+ * Time: 10:42 AM
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+public class PersonArrangeDao extends EntityService<PersonArrange, Long> {
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        initDao(sessionFactory, PersonArrange.class);
+    }
+
+
+}
